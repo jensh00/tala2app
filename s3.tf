e4b6871543b6ddb0d034fb452cdc392e4ebb06aa
capcloud <119628596+jensh00@users.noreply.github.com>
2023-06-23 15:42:06 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,8 +1,8 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "tala2app-s3-bucket"
-  acl    = "public"
+  bucket = "virgo-s3-bucket"
+  acl    = "private"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
